86f10b2c7be9389c6314865e3c0a8103df423845
Jeeban Bagdi <jeeban.bagdi@ingreens.in>
2018-02-06 15:59:48 +0530

    Location fixed...

--- app/src/main/java/com/ingreens/gps/MainActivity.java
+++ app/src/main/java/com/ingreens/gps/MainActivity.java
@@ -1,50 +1,94 @@
 package com.ingreens.gps;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
 
     Button btnShowLocation;
 
-    // GPSTracker class
-    GPSTracker gps;
+    LocationManager locationManager;
+    ProgressDialog dialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        dialog=new ProgressDialog(this);
         btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
 
-        // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+        locationManager=(LocationManager) getSystemService(LOCATION_SERVICE);
 
-            @Override
-            public void onClick(View arg0) {
-                // create class object
-                gps = new GPSTracker(MainActivity.this);
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-                // check if GPS enabled
-                if(gps.canGetLocation()){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
-                }
-
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
             }
-        });
+        }
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+            }
+        }
+        dialog.setTitle("Location");
+        dialog.setMessage("Getting location...");
+        dialog.show();
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "Lat: "+location.getLatitude()+" \nLang: "+location.getLongitude(), Toast.LENGTH_LONG).show();
+        dialog.dismiss();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
